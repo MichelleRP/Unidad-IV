c515bd32908585850e36abaf56e2453f907d146a
MichelleRP <michelle.ruiz07@hotmail.com>
2016-05-26 22:58:34 -0600

    unidad 4
    trabajo de la ultima unidad

---
+++ VolleyMan/app/src/main/java/michelle/volleyman/MainActivity.java
@@ -0,0 +1,30 @@
+package michelle.volleyman;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        adapter = new PostAdapter(this);
+        listView.setAdapter(adapter);
+    }
+
+}
